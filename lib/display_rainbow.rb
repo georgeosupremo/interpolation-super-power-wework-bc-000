cf04e2a2e12a04c3f0c962d0ee241c32f40a2db8
george.burton@wework.com <george.burton@wework.com>
2018-02-11 22:42:39 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 colors = ['red','orange','yellow','green','blue','indigo','violet']
 
 def display_rainbow(colors)
-puts "#{"R"}: #{colors[0]}"
+puts " #{colors[0]}"
 puts "#{"O"}: #{colors[1]}"
 puts "#{"Y"}: #{colors[2]}"
 puts "#{"G"}: #{colors[3]}"
